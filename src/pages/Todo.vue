7ff482f86cfe0f6929e52c41c6b77d0100e69c27
Dylan Attal <attal.dylan@gmail.com>
2020-01-12 16:20:16 -0500

    creates delete button

--- src/pages/Todo.vue
+++ src/pages/Todo.vue
@@ -15,6 +15,9 @@
         <q-item-section>
           <q-item-label>{{ task.title }}</q-item-label>
         </q-item-section>
+        <q-item-section v-if="task.done" side>
+          <q-btn @click.stop="deleteTask(index)" flat round dense color="primary" icon="delete" />
+        </q-item-section>
       </q-item>
     </q-list>
   </q-page>
@@ -39,6 +42,11 @@ export default {
         }
       ]
     };
+  },
+  methods: {
+    deleteTask(index) {
+      this.tasks.splice(index, 1);
+    }
   }
 };
 </script>
