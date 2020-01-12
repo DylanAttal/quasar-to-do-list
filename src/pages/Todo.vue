01e7efde439b61a906c65a94da9276b6928413f0
Dylan Attal <attal.dylan@gmail.com>
2020-01-12 16:15:52 -0500

    adds hardcoded todos

--- src/pages/Todo.vue
+++ src/pages/Todo.vue
@@ -1,9 +1,53 @@
 <template>
-  <q-page>
-    <h5>To Do</h5>
+  <q-page class="bg-grey-3 column">
+    <q-list class="bg-white" separator bordered>
+      <q-item
+        v-for="(task, index) in tasks"
+        :key="index"
+        clickable
+        @click="task.done = !task.done"
+        :class="{ 'done bg-blue-1': task.done }"
+        v-ripple
+      >
+        <q-item-section avatar>
+          <q-checkbox v-model="task.done" class="no-pointer-events" color="primary" />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>{{ task.title }}</q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-list>
   </q-page>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      tasks: [
+        {
+          title: "First task",
+          done: false
+        },
+        {
+          title: "Second task",
+          done: false
+        },
+        {
+          title: "Third task",
+          done: false
+        }
+      ]
+    };
+  }
+};
 </script>
+
+<style lang="scss">
+.done {
+  .q-item__label {
+    text-decoration: line-through;
+    color: #bbb;
+  }
+}
+</style>
